9d5add6c56fcd34fb9adb05f9b5f4ede47ee295e
Kevin Kanzelmeyer <kevin.kanzelmeyer@archarithms.com>
2017-01-26 16:41:05 -0600

    added string test example solution

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,10 @@
 import test from 'ava'
+import { titleCase } from './index'
 
 test('runs tests', t => {
   t.pass()
+})
+
+test('title case test', t => {
+  t.is(titleCase('TITLE_CASE'), 'Title Case')
 })
