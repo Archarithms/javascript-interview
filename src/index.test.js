aedf69f46aabc9a23303e1847a91fe740c0a40d7
Kevin Kanzelmeyer <kevin.kanzelmeyer@archarithms.com>
2017-02-07 10:47:23 -0600

    i did it

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,13 @@
 import test from 'ava'
+import {transform} from './index'
 
 test('runs tests', t => {
   t.pass()
+})
+
+test('transform', t => {
+  t.true(transform('THIS_INPUT') === 'This Input')
+  t.throws(() => {
+    transform(null)
+  }, Error)
 })
