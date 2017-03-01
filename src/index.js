853257f1d073c21f1303b3c04ac4e70cddc64f7b
Anthony Tamez <anthony.tamez@archarithms.com>
2017-03-01 13:26:18 -0600

    added function to convert unix timestamp to date string

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
+import moment from 'moment'
 const isDebug = process.env.NODE_ENV === 'debug'
 
 console.log('Get to coding!')
 console.log(`Debug mode: ${isDebug ? 'on' : 'off'}`)
+
+export const makeDateStringFromUnixTimestamp = (timestamp) => {
+  if (timestamp === undefined) {
+    return moment().format('MMMM D, YYYY')
+  }
+  return moment((timestamp * 1000)).format('MMMM D, YYYY')
+}
--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,13 @@
 import test from 'ava'
+import moment from 'moment'
+import { makeDateStringFromUnixTimestamp } from './index'
 
-test('runs tests', t => {
-  t.pass()
+test('if argument is given, date string is returned', t => {
+  // If an argument is sent to the function, assume it is a unix timestamp
+  // (in seconds) and return the date string.
+  t.is(makeDateStringFromUnixTimestamp(1499144400), 'July 4, 2017')
+})
+
+test('if no argument is given, today\'s date string in returned', t => {
+  t.is(makeDateStringFromUnixTimestamp(), moment().format('MMMM D, YYYY'))
 })
