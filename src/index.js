563d244ae7d5342581fcdb3d9c813d8fd27fca8f
Kevin Kanzelmeyer <kevin.kanzelmeyer@archarithms.com>
2018-02-23 10:33:48 -0600

    updated docs

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
-import moment from 'moment';
 
+/**
+ * CODING CHALLENGE 1
+ * @param {String} str the string to format
+ * @see `codingChallenge.md` for instructions
+ */
 export function titleCase (str) {
-  if (str === null) {
-    throw Error('invalid input');
-  }
-  const splitStr = str.toLowerCase().split(/[^\w]|_/g);
-  for (let i = 0; i < splitStr.length; i += 1) {
-    splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
-  }
-  return splitStr.join(' ').trim();
+  // CODE HERE
 }
 
+/**
+ * CODING CHALLENGE 2
+ * @param {Number} timestamp the timestamp to format
+ * @see `codingChallenge.md` for instructions
+ */
 export function toDateString (timestamp) {
-  if (timestamp === null) {
-    throw Error('invalid input');
-  }
-  return moment(timestamp).format('MMMM DD, YYYY');
+  // CODE HERE
 }
 
