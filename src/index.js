3755d5a4e89c26bc57ad4a78b8c733f24e34dd08
jmsbrett <jamesb@divdesignllc.com>
2021-10-11 13:48:29 -0500

    adding tests for scenerio 1 titleCase minus null return error

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-
 /**
  * CODING CHALLENGE 1
  * @param {String} str the string to format
  * @see `codingChallenge.md` for instructions
  */
-export function titleCase (str) {
+export function titleCase(str) {
   // CODE HERE
-  return false;
+  // try {
+    // if (str != null) {
+      const strReplace = str.replace(/[^a-zA-Z0-9]+/g, " ");
+      const lowCase = strReplace.toLowerCase().trim();
+      const splitWords = lowCase.split(" ");
+
+      for (let i = 0; i < splitWords.length; i++) {
+        splitWords[i] =
+          splitWords[i][0].toUpperCase() + splitWords[i].substr(1);
+      }
+
+      return splitWords.join(" ");
+  //   }
+  // } catch (error) {
+    // throw new Error("Null Input");
+    // error = new Error("Null Input")
+    // console.log(error + " ERROR")
+    // return new Error("Null Input")
+  // }
 }
 
 /**
@@ -14,8 +31,7 @@ export function titleCase (str) {
  * @param {Number} timestamp the timestamp to format
  * @see `codingChallenge.md` for instructions
  */
-export function dateString (timestamp) {
+export function dateString(timestamp) {
   // CODE HERE
   return false;
 }
-
